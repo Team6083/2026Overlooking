316e8de3d2b07af5a706ee3c6492c2cd8563e8e4
GaryLiu0317 <j110362@my.cmsh.cyc.edu.tw>
2026-01-29 16:39:30 +0800

    [ClimberSubsystem]fix:再再次修改import

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -6,8 +6,8 @@ package frc.robot.subsystems;
 
 import com.revrobotics.spark.FeedbackSensor;
 import com.revrobotics.spark.SparkBase.ControlType;
-import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
