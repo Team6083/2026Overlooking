3826e040c09d7ffb9771a46b076479e6cd41cf60
Alan <j110396@my.cmsh.cyc.edu.tw>
2026-01-29 16:54:19 +0800

    intake subsystem final

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -5,38 +5,63 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.Joystick;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 
 public class IntakeSubsystem extends SubsystemBase {
-  /** Creates a new IntakeSubsystem. */
+    SparkMax intakeMotor = new SparkMax(10, MotorType.kBrushless);
+    SparkMaxConfig config = new SparkMaxConfig();
+    Joystick driverJoystick = new Joystick(0);
+
   public IntakeSubsystem() {
+    config.inverted(false)
+          .smartCurrentLimit(30)
+          .idleMode(IdleMode.kBrake);
+    intakeMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
 
   private void intake() {
-
+  
   }
 
   private void reverseIntake() {
-
+    intakeMotor.set(-0.5);
   }
 
   private void stopIntake() {
-
+    intakeMotor.set(0);
   }
 
   private void rotateUp() {
-
+   intakeMotor.set(0.5);
   }
 
   private void rotateDown() {
-
+   intakeMotor.set(-0.5);
   }
 
   private void stopRotate() {
-
+    intakeMotor.set(0);
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    if (driverJoystick.getRawButton(2)) {
+            rotateUp();
+        } 
+        else if (driverJoystick.getRawButton(3)) {
+            rotateDown();
+        } 
+        else {
+            stopRotate();
+            
+    }
+
   }
 }
