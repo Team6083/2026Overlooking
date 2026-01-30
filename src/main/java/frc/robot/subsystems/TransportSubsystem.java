2749a5f12de882bd4f62a24a92c6b95882abab83
alicechen <aliceyichen1110@gmail.com>
2026-01-30 18:21:46 +0800

    [Transport] fix: 改變import順序

--- src/main/java/frc/robot/subsystems/TransportSubsystem.java
+++ src/main/java/frc/robot/subsystems/TransportSubsystem.java
@@ -4,8 +4,9 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 
 
 public class TransportSubsystem extends SubsystemBase {
