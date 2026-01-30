aee521cdab65fa0fae7acc681e03d5ef48b46f7b
GaryLiu0317 <j110362@my.cmsh.cyc.edu.tw>
2026-01-30 16:47:14 +0800

    [ClimberSubsystem]style:修改檔名

--- src/main/java/frc/robot/subsystems/climberCommand.java
+++ src/main/java/frc/robot/subsystems/climberCommand.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ClimberSubsystem;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
-public class climberCommand extends Command {
+public class ClimberCommand extends Command {
   /** Creates a new climberCMD. */
   public enum ClimberAction {
     L1, L2, UP, DOWN
@@ -17,7 +17,7 @@ public class climberCommand extends Command {
   ClimberSubsystem climberSubsystem;
   ClimberAction action;
 
-  public climberCommand(ClimberSubsystem climberSubsystem, ClimberAction action) {
+  public ClimberCommand(ClimberSubsystem climberSubsystem, ClimberAction action) {
     // Use addRequirements() here to declare subsystem dependencies.
     this.climberSubsystem = climberSubsystem;
     this.action = action;
