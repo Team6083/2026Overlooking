c907f865ac962ee7343232ec654db3bb8e7218e3
Alan <j110396@my.cmsh.cyc.edu.tw>
2026-01-30 17:10:01 +0800

    [Intake] feat: add encoder and smartdashboard

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -4,8 +4,6 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
