edbdb303c15566e79b100959a16dad60b6247962
Alan <j110396@my.cmsh.cyc.edu.tw>
2026-02-02 14:30:27 +0800

    [intake] feat : add RobotContainer

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,5 +6,17 @@ package frc.robot;
 
 /** Add your docs here. */
 public class Constants {
-
+  public class IntakeConstants {
+    public static final int intakeMotorId = 10;
+    public static final int pivotLeftId = 11;
+    public static final int pivotRightId = 12;
+    public static final int pivotEncoderId = 4;
+// ---------------------------------- Intake Motor Speeds ----------------------------------
+    public static final double intakeSpeed = 0.5;
+    public static final double reverseIntakeSpeed = -0.5;
+    public static final double stopIntakeSpeed = 0.0;
+    public static final double pivotSpeed = 0.2;
+    public static final double reversePivotSpeed = -0.2;
+    public static final double stopPivotSpeed = 0.0;
+  }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,16 +5,45 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.IntakeSubsystem;
 
 public class RobotContainer {
+  private final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
+  private final CommandXboxController driverController = new CommandXboxController(0);
+
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    
+    // 按住A鍵吸球放開停止
+    driverController.a().whileTrue(
+        new RunCommand(() -> intakeSubsystem.intake(), intakeSubsystem)
+    ).onFalse(new InstantCommand(() -> intakeSubsystem.stopIntake(), intakeSubsystem));
+
+    // 按住B鍵反轉放開停止
+    driverController.b().whileTrue(
+        new RunCommand(() -> intakeSubsystem.reverseIntake(), intakeSubsystem)
+    ).onFalse(new InstantCommand(() -> intakeSubsystem.stopIntake(), intakeSubsystem));
+
+    // ------------------- Pivot 角度控制 -------------------
+
+    // 按住 Y 鍵抬起放開停止
+    driverController.y().whileTrue(
+        new RunCommand(() -> intakeSubsystem.rotateUp(), intakeSubsystem)
+    ).onFalse(new InstantCommand(() -> intakeSubsystem.stopRotate(), intakeSubsystem));
+
+    // 按住 X鍵放下放開停止
+    driverController.x().whileTrue(
+        new RunCommand(() -> intakeSubsystem.rotateDown(), intakeSubsystem)
+    ).onFalse(new InstantCommand(() -> intakeSubsystem.stopRotate(), intakeSubsystem));
+  }
 
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+    return null; 
   }
 }
