1a6f1435c098aa1db78492a7a5fbe709da9d573a
Alan <j110396@my.cmsh.cyc.edu.tw>
2026-02-02 14:50:49 +0800

    [Intake] feat : add RobotContainer

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,13 +5,16 @@
 package frc.robot;
 
 /** Add your docs here. */
-public class Constants {
-  public class IntakeConstants {
+  public final class Constants {
+    
+    public class IntakeConstants {
+
     public static final int intakeMotorId = 10;
     public static final int pivotLeftId = 11;
     public static final int pivotRightId = 12;
     public static final int pivotEncoderId = 4;
-// ---------------------------------- Intake Motor Speeds ----------------------------------
+    // ---------------------------------- Intake Motor Speeds
+    // ----------------------------------
     public static final double intakeSpeed = 0.5;
     public static final double reverseIntakeSpeed = -0.5;
     public static final double stopIntakeSpeed = 0.0;
