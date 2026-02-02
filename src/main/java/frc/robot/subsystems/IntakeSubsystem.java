b5cdea1a2a0f7e0e292e2fc601186c5f10fa25d4
Alan <j110396@my.cmsh.cyc.edu.tw>
2026-02-02 12:14:25 +0800

    [intake] fix : fix dashboard

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -4,39 +4,77 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeSubsystem extends SubsystemBase {
-  /** Creates a new IntakeSubsystem. */
+  private final SparkMax intakeMotor = new SparkMax(10, MotorType.kBrushless);
+  private final SparkMax pivotLeft = new SparkMax(11, MotorType.kBrushless);
+  private final SparkMax pivotRight = new SparkMax(12, MotorType.kBrushless);
+  private final DutyCycleEncoder pivotEncoder = new DutyCycleEncoder(4);
+
+  @SuppressWarnings("removal")
   public IntakeSubsystem() {
+    SparkMaxConfig config = new SparkMaxConfig();
+    config.inverted(false)
+        .smartCurrentLimit(30)
+        .idleMode(IdleMode.kBrake);
+
+    intakeMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+    pivotLeft.configure(config, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+    pivotRight.configure(config, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
   }
 
-  private void intake() {
-
+  public void intake() {
+    intakeMotor.set(0.5);
   }
 
-  private void reverseIntake() {
-
+  public void reverseIntake() {
+    intakeMotor.set(-0.5);
   }
 
-  private void stopIntake() {
-
+  public void stopIntake() {
+    intakeMotor.set(0);
   }
 
-  private void rotateUp() {
-
+  public void rotateUp() {
+    pivotLeft.set(0.2);
+    pivotRight.set(-0.2);
   }
 
-  private void rotateDown() {
-
+  public void rotateDown() {
+    pivotLeft.set(-0.2);
+    pivotRight.set(0.2);
   }
 
-  private void stopRotate() {
+  public void stopRotate() {
+    pivotLeft.set(0);
+    pivotRight.set(0);
+  }
 
+  public void deployIntake() {
+    rotateDown();
+  }
+
+  public void retractIntake() {
+    rotateUp();
+  }
+
+  public double getPivotAbsolutePosition() {
+    return pivotEncoder.get();
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("intakemotorspeed", intakeMotor.get());
+    SmartDashboard.putNumber("Pivot Absolute Pos", getPivotAbsolutePosition());
+    SmartDashboard.putBoolean("Pivot Encoder Connected", pivotEncoder.isConnected());
   }
 }
