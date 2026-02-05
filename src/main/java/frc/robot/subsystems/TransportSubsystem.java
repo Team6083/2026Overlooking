8ca9864a43415102e2d73efe1c170390e706d486
alicechen <aliceyichen1110@gmail.com>
2026-02-05 19:59:47 +0800

    [Transport] style: 刪除多餘的空格

--- src/main/java/frc/robot/subsystems/TransportSubsystem.java
+++ src/main/java/frc/robot/subsystems/TransportSubsystem.java
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class TransportSubsystem extends SubsystemBase {
   /** Creates a new transportSubsystem. */
   VictorSP motorspeed = new VictorSP(0);
-  
+
   public TransportSubsystem() {}
 
   private void transportIn() {
