6cb884a2b6fe08b5347c403f7fe752f9dd939244
YuYue1025 <s1410216@my.cmsh.cyc.edu.tw>
2026-02-07 16:27:18 +0800

    [Vision] style: 刪掉多餘空行

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -9,7 +9,6 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class ShooterSubsystem extends SubsystemBase {
   /** Creates a new ShooterSubsystem. */
   public ShooterSubsystem() {}
-  
 
   private void shoot() {
 
