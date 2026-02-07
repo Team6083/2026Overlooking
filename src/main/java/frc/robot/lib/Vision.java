c799936d1fe021170c71534fa7a9c2faa9e31151
way_x112 <j110102@my.cmsh.cyc.edu.tw>
2026-02-07 15:24:51 +0800

    style: Adjust indentation in Vision class methods for consistency

--- src/main/java/frc/robot/lib/Vision.java
+++ src/main/java/frc/robot/lib/Vision.java
@@ -4,13 +4,15 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class Vision {
-    public double getXVisionSet() {
-        NetworkTable table = NetworkTableInstance.getDefault().getTable("6083_Vision");
-        return table.getDoubleTopic("closest_x").subscribe(0.0).get();
-    }
 
-    public double getYVisionSet() {
-        NetworkTable table = NetworkTableInstance.getDefault().getTable("6083_Vision");
-        return table.getDoubleTopic("closest_y").subscribe(0.0).get();
-    }
+  public double getXVisionSet() {
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("6083_Vision");
+    // 注意：這裡縮排是 4 格 (2+2)
+    return table.getDoubleTopic("closest_x").subscribe(0.0).get();
+  } // 這裡縮排是 2 格 (與方法開頭對齊)
+
+  public double getYVisionSet() {
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("6083_Vision");
+    return table.getDoubleTopic("closest_y").subscribe(0.0).get();
+  }
 }
