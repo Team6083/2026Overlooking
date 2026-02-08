206838ece417a1c4fe7ed1eccd5b40e49bf53ef4
蔡采倢 <janet202202@gmail.com>
2026-02-08 16:17:53 +0800

    [Shooter] feat: add target velocity to Constants and fix import formatting

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -4,15 +4,16 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 import frc.robot.Constants.ShooterConstants;
 
 public class ShooterSubsystem extends SubsystemBase {
@@ -21,13 +22,13 @@ public class ShooterSubsystem extends SubsystemBase {
   }
 
   private final VictorSPX shooterMotor = new VictorSPX(ShooterConstants.shooterMotorID);
-  private final Encoder shooterEncoder = new Encoder(ShooterConstants.encoderChannelA, ShooterConstants.encoderChannelB);
-  private final SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(ShooterConstants.feedforwardKs, ShooterConstants.feedforwardKv, ShooterConstants.feedforwardKa);
-
+  private final Encoder shooterEncoder = new Encoder(ShooterConstants.encoderChannelA,
+      ShooterConstants.encoderChannelB);
+  private final SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(ShooterConstants.feedforwardKs,
+      ShooterConstants.feedforwardKv, ShooterConstants.feedforwardKa);
 
   private void shoot() {
-    //shooterMotor.set(ShooterConstants.shooterMotorSpeed);
-    double targetVelocity = 4000;
+    double targetVelocity = ShooterConstants.targetVelocity;
     double feedforwardVoltage = feedforward.calculate(targetVelocity);
     shooterMotor.set();
   }
