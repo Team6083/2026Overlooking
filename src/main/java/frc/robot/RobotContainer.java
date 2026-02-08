1c1365f8f2c812ba56988f28594af3399ecd687c
tsen991007 <tsen.991007@gmail.com>
2026-02-08 10:02:12 +0800

    [Drive] feat: SwerveModule

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,17 +4,39 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.driveBase.SwerveModule;
 
 public class RobotContainer {
-  public RobotContainer() {
+  private final SwerveModule m_testModule = new SwerveModule(1, 2, 9, 0.0, false, false);
+  private final CommandXboxController m_driverController = new CommandXboxController(0);
+
+  public RobotContainer() {    
     configureBindings();
-  }
+
+    m_testModule.setDefaultCommand(
+        new RunCommand(() -> {
+            double speed = -MathUtil.applyDeadband(m_driverController.getLeftY(), 0.1) * 2.0; 
+            
+            Rotation2d angle = Rotation2d.fromDegrees(
+                MathUtil.applyDeadband(m_driverController.getRightX(), 0.1) * 180
+            );
+            
+            SwerveModuleState desiredState = new SwerveModuleState(speed, angle);
+            m_testModule.setDesiredState(desiredState);
+        }, m_testModule) 
+    );
+  } 
 
   private void configureBindings() {}
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
   }
-}
+} 
