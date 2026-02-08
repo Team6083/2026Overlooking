7d23aa7e6151233d03539dd2eb0cf86ebf42e757
tsen991007-bit <tsen.991007@gmail.com>
2026-02-08 18:24:23 +0800

    [Drive] style: 把package 名稱 driveBase 改成 drivebase

---
+++ src/main/java/frc/robot/drivebase/SwerveDrive.java
@@ -0,0 +1,25 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.driveBase;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class SwerveDrive extends SubsystemBase {
+  /** Creates a new SwerveDrive. */
+  public SwerveModule m_testModule = new SwerveModule(
+      23, 24, 34, 0.928711, true, false);
+
+  public SwerveDrive() {
+  }
+
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber("angle", m_testModule.getAngleRadians());
+    SmartDashboard.putData(m_testModule.pid);
+    SmartDashboard.putNumber("motorOutput", m_testModule.turningMotor.get());
+    // This method will be called once per scheduler run
+  }
+}
