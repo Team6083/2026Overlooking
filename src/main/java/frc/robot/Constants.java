a17daf8128df19dc1e4472987c603a6543c57237
蔡采倢 <janet202202@gmail.com>
2026-02-08 15:50:39 +0800

    [Shooter] style: 調整縮排

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,15 +6,15 @@ package frc.robot;
 
 /** Add your docs here. */
 public class Constants {
-    public static final class ShooterConstants {
-        public static final int shooterMotorID = 0;
-        public static final double shooterMotorSpeed = 0.6;
-        public static final double feedforwardKs = 0;
-        public static final double feedforwardKv = 0;
-        public static final double feedforwardKa = 0;
-        public static final int encoderChannelA = 0;
-        public static final int encoderChannelB = 0;
+  public static final class ShooterConstants {
+    public static final int shooterMotorID = 0;
+    public static final double shooterMotorSpeed = 0.6;
+    public static final double feedforwardKs = 0;
+    public static final double feedforwardKv = 0;
+    public static final double feedforwardKa = 0;
+    public static final int encoderChannelA = 0;
+    public static final int encoderChannelB = 0;
 
-    }
+  }
 
 }
