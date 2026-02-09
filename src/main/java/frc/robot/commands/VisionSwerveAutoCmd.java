4549b3fe308680762316cc9df383a458c297f6be
way_x112 <j110102@my.cmsh.cyc.edu.tw>
2026-02-09 16:46:42 +0800

    [Vision] fix: import順序

--- src/main/java/frc/robot/commands/VisionSwerveAutoCmd.java
+++ src/main/java/frc/robot/commands/VisionSwerveAutoCmd.java
@@ -4,8 +4,8 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.lib.Vision;
 import frc.robot.subsystems.swervedrive.SwerveDrive;
