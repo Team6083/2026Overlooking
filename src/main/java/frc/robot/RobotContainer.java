a890c37bcf81c5d638f7653bf6a2ceaae157bc2e
Elena <j110335@my.cmsh.cyc.edu.tw>
2026-02-10 12:31:04 +0800

    [auto] feat: 新增路徑

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,12 +4,17 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 public class RobotContainer {
+  private final SendableChooser<String> m_Chooser = new SendableChooser<>();
+
   public RobotContainer() {
     configureBindings();
+
+    m_Chooser.addOption("blueLeftAuto", "blueLeftAuto");
   }
 
   private void configureBindings() {}
