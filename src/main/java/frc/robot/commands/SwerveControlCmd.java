55341335ed33dfb17f76686139761452e79e67b9
Hannahjjj97 <hannahjjj97@gmail.com>
2026-02-11 20:14:20 +0800

    [Swerve] fix: 因為 interface 沒有取得最大速度的方法 所以我直接改成數字 4

--- src/main/java/frc/robot/commands/SwerveControlCmd.java
+++ src/main/java/frc/robot/commands/SwerveControlCmd.java
@@ -46,11 +46,11 @@ public class SwerveControlCmd extends Command {
     magnification = mainController.leftBumper().getAsBoolean() ? 0.6 : 0.3;
     rotMagnification = mainController.leftBumper().getAsBoolean() ? 0.6 : 0.3;
 
-    speedX = -limiterX.calculate(MathUtil.applyDeadband(mainController.getLeftY(), 0.1)) * yagslSwerve.getMaxSpeed()
+    speedX = -limiterX.calculate(MathUtil.applyDeadband(mainController.getLeftY(), 0.1)) * 4
         * magnification;
-    speedY = -limiterY.calculate(MathUtil.applyDeadband(mainController.getLeftX(), 0.1)) * yagslSwerve.getMaxSpeed()
+    speedY = -limiterY.calculate(MathUtil.applyDeadband(mainController.getLeftX(), 0.1)) * 4
         * magnification;
-    rotSpeed = rotLimiter.calculate(MathUtil.applyDeadband(mainController.getRightX(), 0.1)) * yagslSwerve.getMaxSpeed()
+    rotSpeed = rotLimiter.calculate(MathUtil.applyDeadband(mainController.getRightX(), 0.1)) * 4
         * rotMagnification;
     swerveDrive.drive(speedX, speedY, rotSpeed, true);
   }
