ec61518d01f967af928977cf1e056bf12c1a3a3c
Hannahjjj97 <hannahjjj97@gmail.com>
2026-02-11 21:01:33 +0800

    [Swerve] style: 刪除 import

--- src/main/java/frc/robot/commands/SwerveControlCmd.java
+++ src/main/java/frc/robot/commands/SwerveControlCmd.java
@@ -9,7 +9,6 @@ import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.swervedrive.SwerveDrive;
-import frc.robot.subsystems.swervedrive.YagslSwerve;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class SwerveControlCmd extends Command {
