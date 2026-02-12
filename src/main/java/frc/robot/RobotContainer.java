9462869e345dff94ed9700338f12d7f685dfd4f9
Hannahjjj97 <hannahjjj97@gmail.com>
2026-02-12 08:20:31 +0800

    [RobotContainer] feat: 移除測試用的程式

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,32 +4,16 @@
 
 package frc.robot;
 
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.drivebase.SwerveDrive;
 
 public class RobotContainer {
-  private final SwerveDrive swerveDrive = new SwerveDrive();
-  private final CommandXboxController driverController = new CommandXboxController(0);
 
   public RobotContainer() {
     configureBindings();
-
-    swerveDrive.setDefaultCommand(
-        new RunCommand(
-            () -> swerveDrive.drive(
-                -MathUtil.applyDeadband(driverController.getLeftY(), 0.1),
-                -MathUtil.applyDeadband(driverController.getLeftX(), 0.1),
-                -MathUtil.applyDeadband(driverController.getRightX(), 0.1),
-                true),
-            swerveDrive));
   }
 
   private void configureBindings() {
-    driverController.start().onTrue(swerveDrive.resetGyroCmd());
   }
 
   public Command getAutonomousCommand() {
