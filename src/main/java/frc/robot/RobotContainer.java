ede9d95b9a2aa08cc5f94a07d7fb79106d1d832d
Maureen1031 <j120380@my.cmsh.cyc.edu.tw>
2026-02-12 15:53:18 +0800

    [auto] feat: 選路徑的程式

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -77,6 +77,6 @@ public class RobotContainer {
   }
 
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+    return autoChooser.getSelected();
   }
 }
