7f7377cb200aa26b1bcb811019f49bb8a633df76
SallyLin0907 <ples104013@gmail.com>
2026-02-12 09:50:55 +0800

    [configureBinding] feat: 新增搖桿按鍵

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,7 +8,11 @@ import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commands.ShooterComboCmd;
 import frc.robot.commands.SwerveControlCmd;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.TransportSubsystem;
 import frc.robot.subsystems.swervedrive.SwerveDrive;
 import frc.robot.subsystems.swervedrive.YagslSwerve;
 import java.io.File;
@@ -16,15 +20,30 @@ import java.io.File;
 public class RobotContainer {
   private final SwerveDrive swerveDrive;
   private final CommandXboxController mainController = new CommandXboxController(0);
+  private final ShooterSubsystem shooterSubsystem;
+  private final TransportSubsystem transportSubsystem;
+  private final IntakeSubsystem intakeSubsystem;
 
   public RobotContainer() {
     swerveDrive = new YagslSwerve(new File(Filesystem.getDeployDirectory(), "swerve"));
+    shooterSubsystem = new ShooterSubsystem();
+    transportSubsystem = new TransportSubsystem();
+    intakeSubsystem = new IntakeSubsystem();
     configureBindings();
+
   }
 
   private void configureBindings() {
     swerveDrive.setDefaultCommand(new SwerveControlCmd(swerveDrive, mainController));
-    mainController.button(8).onTrue(swerveDrive.zeroGyroCommand());
+    mainController.start().onTrue(swerveDrive.zeroGyroCommand());
+    mainController.a().whileTrue(new ShooterComboCmd(shooterSubsystem, transportSubsystem));
+    mainController.x().toggleOnTrue(shooterSubsystem.shootCmd());
+    mainController.b().whileTrue(transportSubsystem.transportInCmd());
+    mainController.povDown().onTrue(intakeSubsystem.deployIntakeCmd());
+    mainController.povLeft().whileTrue(intakeSubsystem.manualDeployIntakeCmd());
+    mainController.povDownRight().whileTrue(intakeSubsystem.manualRetractCmd());
+    mainController.rightTrigger().whileTrue(intakeSubsystem.intakeCmd());
+    mainController.leftTrigger().whileTrue(intakeSubsystem.reverseIntakeCmd());
   }
 
   public Command getAutonomousCommand() {
