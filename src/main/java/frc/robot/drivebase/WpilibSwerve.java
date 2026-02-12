ee1416cc02555b969651cd990deaa523c00ed00b
tsen991007 <tsen.991007@gmail.com>
2026-02-13 06:13:27 +0800

    [Swerve] style: 刪掉多餘的空行

--- src/main/java/frc/robot/drivebase/WpilibSwerve.java
+++ src/main/java/frc/robot/drivebase/WpilibSwerve.java
@@ -5,7 +5,6 @@
 package frc.robot.drivebase;
 
 import java.util.function.Supplier;
-
 import com.studica.frc.AHRS;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
