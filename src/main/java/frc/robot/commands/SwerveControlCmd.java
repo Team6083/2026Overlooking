056321ef595cf286042aba2eda83a27fb90b59a3
YiTsen1007 <j120409@my.cmsh.cyc.edu.tw>
2026-02-20 14:22:49 +0800

    [Swerve] value: 把搖桿數值改成負負負

--- src/main/java/frc/robot/commands/SwerveControlCmd.java
+++ src/main/java/frc/robot/commands/SwerveControlCmd.java
@@ -47,9 +47,9 @@ public class SwerveControlCmd extends Command {
 
     speedX = -limiterX.calculate(MathUtil.applyDeadband(mainController.getLeftY(), 0.1)) * 4
         * magnification;
-    speedY = - limiterY.calculate(MathUtil.applyDeadband(mainController.getLeftX(), 0.1)) * 4
+    speedY = -limiterY.calculate(MathUtil.applyDeadband(mainController.getLeftX(), 0.1)) * 4
         * magnification;
-    rotSpeed = rotLimiter.calculate(MathUtil.applyDeadband(mainController.getRightX(), 0.1))
+    rotSpeed = -rotLimiter.calculate(MathUtil.applyDeadband(mainController.getRightX(), 0.1))
         * 4 * rotMagnification;
     swerveDrive.drive(speedX, speedY, rotSpeed, true);
   }
