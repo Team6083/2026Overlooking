adfebe5265a26104a743e19516ac7e04e7f3bfd2
SallyLin0907 <ples104013@gmail.com>
2026-02-21 16:06:59 +0800

    refactor: AutoShootCommandGroup 移到 commandgroups 資料夾下

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commandgroups.AutoShootCommandGroup;
 import frc.robot.commands.ShooterComboCmd;
 import frc.robot.commands.SwerveControlCmd;
 import frc.robot.lib.TagTracking;
