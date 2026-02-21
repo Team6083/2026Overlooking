525a9c4d537cf79aa05f51d68fb1524ceeb9150b
SallyLin0907 <ples104013@gmail.com>
2026-02-21 15:47:05 +0800

    [TagTracking] feat: 新增 tx 的對準、調 PID

--- src/main/java/frc/robot/commands/AutoAlignCmd.java
+++ src/main/java/frc/robot/commands/AutoAlignCmd.java
@@ -3,52 +3,67 @@ package frc.robot.commands;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.lib.TagTracking;
 import frc.robot.subsystems.swervedrive.SwerveDrive;
 
 public class AutoAlignCmd extends Command {
 
-  private final TagTracking vision;
+  private final TagTracking tagTracking;
   private final SwerveDrive drive;
   private final PIDController yawPID;
-  private final PIDController distPID; 
+  private final PIDController tzPID;
+  private final PIDController txPID;
+
   private final Debouncer targetDebouncer;
   private boolean isTargetValid;
 
   public AutoAlignCmd(TagTracking vision, SwerveDrive drive) {
-    this.vision = vision;
+    this.tagTracking = vision;
     this.drive = drive;
     this.yawPID = new PIDController(0.05, 0, 0);
-    yawPID.setTolerance(1.5);
-    this.distPID = new PIDController(0.05, 0, 0);
-    distPID.setTolerance(0.15);
+    yawPID.setTolerance(1);
+    this.tzPID = new PIDController(1, 0, 0.1);
+    tzPID.setTolerance(0.15);
+    this.txPID = new PIDController(1, 0, 0.1);
+    txPID.setTolerance(0.15);
     this.targetDebouncer = new Debouncer(0.1, Debouncer.DebounceType.kFalling);
     addRequirements(drive);
+
+    SmartDashboard.putBoolean("TagTrackingHasTag", vision.hasTarget());
+    SmartDashboard.putBoolean("TagTrackingIsHubTag", vision.isHubTag());
+    SmartDashboard.putData("yawPID", yawPID);
+    SmartDashboard.putData("TzPID", tzPID);
+    SmartDashboard.putData("TxPID", txPID);
   }
-  
+
   @Override
   public void initialize() {
     yawPID.reset();
-    distPID.reset();
+    tzPID.reset();
   }
 
   @Override
   public void execute() {
-    isTargetValid = targetDebouncer.calculate(vision.hasTarget() && vision.isHubTag());
+    isTargetValid = targetDebouncer.calculate(tagTracking.hasTarget() && tagTracking.isHubTag());
 
     double yawOutput = 0;
-    double distOutput = 0;
+    double tzOutput = 0;
+    double txOutput = 0;
 
     if (isTargetValid) {
-      yawOutput = yawPID.calculate(vision.get3dYaw(), 0);
-      yawOutput = MathUtil.clamp(yawOutput, -0.5, 0.5);
-        
-      distOutput = distPID.calculate(vision.get3dTz(), 1.2);
-      distOutput = MathUtil.clamp(distOutput, -0.4, 0.4);
+      yawOutput = -yawPID.calculate(tagTracking.get3dYaw(), 0);
+      yawOutput = MathUtil.clamp(yawOutput, -1, 1);
 
-      drive.drive(0, distOutput, yawOutput, false);
-    } 
+      tzOutput = -tzPID.calculate(tagTracking.get3dTz(), 1.7);
+      tzOutput = MathUtil.clamp(tzOutput, -1.5, 1.5);
+
+      txOutput = txPID.calculate(tagTracking.get3dTx(), 0);
+      txOutput = MathUtil.clamp(txOutput, -1.5, 1.5);
+
+      drive.drive(tzOutput, txOutput, yawOutput, false);
+    }
   }
 
   @Override
@@ -56,7 +71,7 @@ public class AutoAlignCmd extends Command {
     if (!isTargetValid) {
       return true;
     }
-    return yawPID.atSetpoint() && distPID.atSetpoint();
+    return yawPID.atSetpoint() && tzPID.atSetpoint();
   }
 
   @Override
