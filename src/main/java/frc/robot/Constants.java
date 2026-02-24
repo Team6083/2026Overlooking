bc5c29925e50843f43d00260ee97f48f5a4c6d64
yue1025 <smallyueyu@gmail.com>
2026-02-24 22:44:29 +0800

    [Vision] value: 調VisionConstants的參數

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -76,6 +76,6 @@ public class Constants {
     // 底盤里程計的預期誤差大小 (X, Y, Theta)
     public static final Vector<N3> stateStdDevs = VecBuilder.fill(0.1, 0.1, 0.1);
     // 視覺數據的預期誤差大小 (X, Y, Theta)
-    public static final Vector<N3> visionStdDevs = VecBuilder.fill(0.7, 0.7, 0.7);
+    public static final Vector<N3> visionStdDevs = VecBuilder.fill(0.9, 0.9, 0.9);
   }
 }
