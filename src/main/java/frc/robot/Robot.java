c52d2d6cb5d3d78cc1e58a40176ec400cec0f5ac
Hannahjjj97 <hannahjjj97@gmail.com>
2026-02-24 21:37:11 +0800

    [Auto] feat: 把在自動路徑開始跑之前 reset gyro 的方法拿掉

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -47,7 +47,6 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
-    m_robotContainer.autoInit();
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     if (m_autonomousCommand != null) {
