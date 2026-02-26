aa6c0064757f1fe2eca1311ef4d2a377d3d4cb79
Hannahjjj97 <hannahjjj97@gmail.com>
2026-02-26 09:05:50 +0800

    [Shooter] value: Kv 調小，他現在變得蠻準的，誤差大約 100 以內 目標速度調高至 5700，是目前測試出來的最高轉速

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -76,11 +76,11 @@ public class Constants {
     public static final int shooterMotorID = 35;
     public static final double shooterMotorSpeed = 0.6;
     public static final double feedforwardKs = 0.2;
-    public static final double feedforwardKv = 0.0028;
+    public static final double feedforwardKv = 0.002;
     public static final double feedforwardKa = 0;
     public static final int encoderChannelA = 0;
     public static final int encoderChannelB = 1;
-    public static final double targetVelocity = 4000;
+    public static final double targetVelocity = 5700;
   }
 
   public static class TransportConstants {
