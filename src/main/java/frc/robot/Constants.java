0ea2ea11c91e5c59cd0a4e0bc86bb1542d053f63
SallyLin0907 <ples104013@gmail.com>
2026-03-01 22:22:20 +0800

    [Auto] value: kpTranslation 0.03 變 0.02

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -90,7 +90,7 @@ public class Constants {
   }
 
   public static class AutoConstants {
-    public static final double kpTranslation = 0.03;
+    public static final double kpTranslation = 0.02;
     public static final double kiTranslation = 0.0;
     public static final double kdTranslation = 0;
     public static final double kpRotation = 4;
