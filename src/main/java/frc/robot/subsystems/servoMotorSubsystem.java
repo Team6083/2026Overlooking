1456c4aa14f87fe687d534efe4c7222314c6c17a
SallyLin0907 <ples104013@gmail.com>
2026-03-02 14:08:41 +0800

    [Servo] feat: 寫兩個方法，一個到0度，一個到90度

---
+++ src/main/java/frc/robot/subsystems/servoMotorSubsystem.java
@@ -0,0 +1,30 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class servoMotorSubsystem extends SubsystemBase {
+  /** Creates a new servoMotorSubsystem. */
+  private final Servo servoMotor;
+
+  public servoMotorSubsystem() {
+    servoMotor = new Servo(0);
+  }
+
+  public void setAngleTo90() {
+    servoMotor.setAngle(90);
+  }
+
+  public void setAngleTo0() {
+    servoMotor.setAngle(0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
