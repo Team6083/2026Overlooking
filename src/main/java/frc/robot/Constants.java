7ab2ab05b5f987c9bcc650299e3a6f9d9eb0b41a
Hannahjjj97 <hannahjjj97@gmail.com>
2026-03-03 18:46:09 +0800

    [Swerve] value: angle offset

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -39,13 +39,13 @@ public class Constants {
 
   public static final DriveBaseConstant CHASSIS_CONFIG = new DriveBaseConstant(
       new SwerveModuleConstant(
-          25, 27, 31, -0.653809 , true, true, "FrontLeft"),
+          25, 27, 31, 0.352783 , true, true, "FrontLeft"),
       new SwerveModuleConstant(
-          26, 28, 32, -0.527100, true, true, "FrontRight"),
+          26, 28, 32, 0.493652, true, true, "FrontRight"),
       new SwerveModuleConstant(
-          24, 23, 34, -0.704590 , true, true, "BackLeft"),
+          24, 23, 34, -0.442871 , true, true, "BackLeft"),
       new SwerveModuleConstant(
-          22, 21, 33, -0.430908, true, true, "BackRight"));
+          22, 21, 33, 0.299072, true, true, "BackRight"));
 
   public static final class ModuleConstant {
     // define the radius of the wheel in meters
