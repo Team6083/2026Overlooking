5092fde2c028925a85c2c5427717130371c37546
yue1025 <smallyueyu@gmail.com>
2026-03-06 18:54:25 +0800

    [AimAsist] style: 修checkstyle warning

--- src/main/java/frc/robot/commands/SwerveControlCmd.java
+++ src/main/java/frc/robot/commands/SwerveControlCmd.java
@@ -33,6 +33,7 @@ public class SwerveControlCmd extends Command {
   private double speedY;
   private double rotSpeed;
   private Supplier<Boolean> shouldSprint;
+  private ChassisSpeeds driveSpeeds;
 
   /** Creates a new SwerveControlCmd. */
   public SwerveControlCmd(SwerveDrive swerveDrive, CommandXboxController mainController,
@@ -97,7 +98,7 @@ public class SwerveControlCmd extends Command {
       double dx = hub[0] - robotPose.getX();
       double dy = hub[1] - robotPose.getY();
       double targetAngle = Math.toDegrees(Math.atan2(dy, dx)) + 180;
-      ChassisSpeeds driveSpeeds = swerveDrive.getRobotRelativeSpeeds();
+      driveSpeeds = swerveDrive.getRobotRelativeSpeeds();[]
 
       double currentAngle = robotPose.getRotation().getDegrees();
       double error = targetAngle - currentAngle;
