135e0b1e9bccf5fbfcfadef282147bf495f92c9c
way_x112 <j110102@my.cmsh.cyc.edu.tw>
2026-03-07 11:56:10 +0800

    Merge branch 'main' into ClimberV2

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -64,73 +64,73 @@ public class Constants {
     public static final double climberKp = 0.1;
     public static final double climberKi = 0.0;
     public static final double climberKd = 0.0;
+  }
 
-    public static final class SwerveControlConstants {
-      public static final double kFastMagnification = 0.6;
-      public static final double kSlowMagnification = 0.3;
-      public static final double kFastRotMagnification = 0.8;
-      public static final double kSlowRotMagnification = 0.4;
-    }
+  public static final class SwerveControlConstants {
+    public static final double kFastMagnification = 0.6;
+    public static final double kSlowMagnification = 0.3;
+    public static final double kFastRotMagnification = 0.8;
+    public static final double kSlowRotMagnification = 0.4;
+  }
 
-    public static final class IntakeConstants {
-      public static final int intakeMotorId = 34;
-      public static final int pivotLeftId = 30;
-      public static final int pivotRightId = 31;
-      public static final int pivotLeftEncoderId = 3;
-      public static final int pivotRightEncoderId = 2;
+  public static final class IntakeConstants {
+    public static final int intakeMotorId = 34;
+    public static final int pivotLeftId = 30;
+    public static final int pivotRightId = 31;
+    public static final int pivotLeftEncoderId = 3;
+    public static final int pivotRightEncoderId = 2;
 
-      public static final double pivotLeftExpectedZero = 70;
-      public static final double pivotRightExpectedZero = 279;
+    public static final double pivotLeftExpectedZero = 70;
+    public static final double pivotRightExpectedZero = 279;
 
-      public static final double pivotEncoderFullRange = 360;
-      public static final double pivotDeployStopPosition = 104;
-      public static final double pivotRetractStopPosition = 4;
+    public static final double pivotEncoderFullRange = 360;
+    public static final double pivotDeployStopPosition = 104;
+    public static final double pivotRetractStopPosition = 4;
 
-      public static final boolean intakeInverted = true;
+    public static final boolean intakeInverted = true;
 
-      public static final double intakeSpeed = 0.6;
+    public static final double intakeSpeed = 0.6;
 
-      public static final double reverseIntakeSpeed = -0.6;
+    public static final double reverseIntakeSpeed = -0.6;
 
-      public static final double deployPivotSpeed = 0.7;
-      public static final double reversePivotSpeed = -0.4;
+    public static final double deployPivotSpeed = 0.7;
+    public static final double reversePivotSpeed = -0.4;
 
-      public static final double pivotManualSpeed = 0.4;
+    public static final double pivotManualSpeed = 0.4;
 
-      public static final double pivotFollowKp = 0.1;
-      public static final double pivotFollowKi = 0;
-      public static final double pivotFollowKd = 0;
+    public static final double pivotFollowKp = 0.1;
+    public static final double pivotFollowKi = 0;
+    public static final double pivotFollowKd = 0;
 
-      public static final boolean motorLeftInverted = false;
-      public static final boolean motorRightInverted = true;
-      public static final boolean encoderLeftInverted = true;
-      public static final boolean encoderRightInverted = false;
-    }
+    public static final boolean motorLeftInverted = false;
+    public static final boolean motorRightInverted = true;
+    public static final boolean encoderLeftInverted = true;
+    public static final boolean encoderRightInverted = false;
+  }
 
-    public static final class ShooterConstants {
-      public static final int shooterMotorID = 35;
-      public static final double shooterMotorSpeed = 0.6;
-      public static final double feedforwardKs = 0.2;
-      public static final double feedforwardKv = 0.002;
-      public static final double feedforwardKa = 0;
-      public static final int encoderChannelA = 0;
-      public static final int encoderChannelB = 1;
-      public static final double targetVelocity = 5700;
-    }
+  public static final class ShooterConstants {
+    public static final int shooterMotorID = 35;
+    public static final double shooterMotorSpeed = 0.6;
+    public static final double feedforwardKs = 0.2;
+    public static final double feedforwardKv = 0.002;
+    public static final double feedforwardKa = 0;
+    public static final int encoderChannelA = 0;
+    public static final int encoderChannelB = 1;
+    public static final double targetVelocity = 5700;
+  }
 
-    public static class TransportConstants {
-      public static final int transportMotorID = 32;
-      public static final double transportMotorIn = 0.2;
-      public static final double transportMotorOut = 0.2;
-    }
+  public static class TransportConstants {
+    public static final int transportMotorID = 32;
+    public static final double transportMotorIn = 0.2;
+    public static final double transportMotorOut = 0.2;
+  }
 
-    public static class AutoConstants {
-      public static final double kpTranslation = 5.75;
-      public static final double kiTranslation = 0.0;
-      public static final double kdTranslation = 0.75;
-      public static final double kpRotation = 4.5;
-      public static final double kiRotation = 0.0;
-      public static final double kdRotation = 0.5;
-    }
+  public static class AutoConstants {
+    public static final double kpTranslation = 5.75;
+    public static final double kiTranslation = 0.0;
+    public static final double kdTranslation = 0.75;
+    public static final double kpRotation = 4.5;
+    public static final double kiRotation = 0.0;
+    public static final double kdRotation = 0.5;
   }
 }
