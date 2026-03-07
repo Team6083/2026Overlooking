b0de26664761fe67992ac52dd3ef5bd3a7a20bfa
YiTsen1007 <j120409@my.cmsh.cyc.edu.tw>
2026-03-07 18:38:06 +0800

    [Swerve] value: 調正式機 offset

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -39,13 +39,13 @@ public class Constants {
 
   public static final DriveBaseConstant COMPETITION_CONFIG = new DriveBaseConstant(
       new SwerveModuleConstant(
-          25, 27, 11, 0.352783, true, true, "FrontLeft"),
+          25, 27, 11, -0.332275, true, true, "FrontLeft"),
       new SwerveModuleConstant(
-          26, 28, 13, 0.493652, true, true, "FrontRight"),
+          26, 28, 13, -0.490967, true, true, "FrontRight"),
       new SwerveModuleConstant(
-          24, 23, 12, -0.442871, true, true, "BackLeft"),
+          24, 23, 12, 0.442627, true, true, "BackLeft"),
       new SwerveModuleConstant(
-          22, 20, 14, 0.299072, true, true, "BackRight"));
+          22, 21, 14, -0.295410, true, true, "BackRight"));
 
   public static final class ModuleConstant {
     // define the radius of the wheel in meters
