d05781d983e535f80b9c4d5561f53b1e811aecea
Elena <j110335@my.cmsh.cyc.edu.tw>
2026-03-13 11:11:05 +0800

    [Intake] value: intake encoder expected zero

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -77,8 +77,8 @@ public class Constants {
     public static final int pivotLeftEncoderId = 3;
     public static final int pivotRightEncoderId = 2;
 
-    public static final double pivotLeftExpectedZero = -306;
-    public static final double pivotRightExpectedZero = 141;
+    public static final double pivotLeftExpectedZero = -37;
+    public static final double pivotRightExpectedZero = 51;
 
     public static final double pivotEncoderFullRange = 360;
     public static final double pivotDeployStopPosition = 130;
