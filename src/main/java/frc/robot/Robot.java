ebf8f79ed6ea542145394545d010ee272f1f1630
Elena <j110335@my.cmsh.cyc.edu.tw>
2026-03-13 10:48:33 +0800

    [Log] value: 自動存 log 打開

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -19,7 +19,7 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private final RobotContainer m_robotContainer;
-  private boolean savelog = false;
+  private boolean savelog = true;
 
   private final NetworkTableInstance ntInstance = NetworkTableInstance.getDefault();
 
