ac9ca3876f097d8da3ef4f1a4dbe9533b58534aa
Elena <j110335@my.cmsh.cyc.edu.tw>
2026-03-15 11:00:11 +0800

    [Auto] fix: 調整射球和 depot 的位置

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -108,7 +108,7 @@ public class RobotContainer {
   }
 
   private void registerCommand() {
-    NamedCommands.registerCommand("deployIntake", intakeSubsystem.deployIntakeCmd().);
+    NamedCommands.registerCommand("deployIntake", intakeSubsystem.deployIntakeCmd());
     NamedCommands.registerCommand("intake", intakeSubsystem.intakeCmd());
     NamedCommands.registerCommand("previousShoot", shooterSubsystem.shootCmd());
     NamedCommands.registerCommand("shoot",
