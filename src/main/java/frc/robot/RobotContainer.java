5425c664746a0b534e5f5d2829bb44059d2eabd6
Hannahjjj97 <hannahjjj97@gmail.com>
2026-03-15 11:35:49 +1100

    [RobotContainer] style: 刪除無用 import

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,8 +6,6 @@ package frc.robot;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
-import com.pathplanner.lib.pathfinding.LocalADStar;
-
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.networktables.NetworkTableInstance;
